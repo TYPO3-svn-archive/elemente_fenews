c3ae874ed64311ec0fc2e2a3ebc95c269abfb064
commodore <commodore@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-19 16:35:58 +0000

    v1.0.15
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/elemente_fenews/tags/v1.0.15@48887 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++
@@ -1,7 +0,0 @@
-#
-# Table structure for table 'tt_news'
-#
-CREATE TABLE tt_news (
-	tx_elementefenews_feuser int(11) DEFAULT '0' NOT NULL,
-	tx_elementefenews_author tinyint(4) DEFAULT '0' NOT NULL,
-);
