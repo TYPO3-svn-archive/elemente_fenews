c897cfb2ad803a32d23c92c25489a487ff3ed051
commodore <commodore@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-19 16:27:02 +0000

    v1.0.15
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/elemente_fenews/tags/v1.0.15@48884 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "elemente_fenews".
+#
+# Auto generated 19-06-2011 11:13
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Frontend News',
+	'description' => 'Frontend news submitter with RTE support (htmlArea RTE) and upload capabilities. Supports CAPTCHA (captcha, sr_freecap) for none access protected sites and DAM references (dam_ttnews) for image and file upload.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.0.14',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Andre Steiling',
+	'author_email' => 'steiling@elemente.ms',
+	'author_company' => 'elemente websolutions',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'tt_news' => '2.5.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"f40c";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"17b7";s:17:"ext_localconf.php";s:4:"1d88";s:14:"ext_tables.php";s:4:"7329";s:14:"ext_tables.sql";s:4:"1b6d";s:15:"flexform_ds.xml";s:4:"e952";s:17:"flexform_ds30.xml";s:4:"7cfd";s:13:"locallang.xml";s:4:"fb38";s:16:"locallang_db.xml";s:4:"a1cf";s:17:"locallang_tca.xml";s:4:"f548";s:14:"doc/manual.sxw";s:4:"b2ad";s:40:"hooks/class.tx_elementefenews_ttnews.php";s:4:"07b8";s:39:"lib/class.tx_elementefenews_version.php";s:4:"e688";s:45:"modfunc1/class.tx_elementefenews_modfunc1.php";s:4:"e21c";s:22:"modfunc1/locallang.xml";s:4:"1b11";s:14:"pi1/ce_wiz.gif";s:4:"2d8a";s:35:"pi1/class.tx_elementefenews_pi1.php";s:4:"b857";s:43:"pi1/class.tx_elementefenews_pi1_wizicon.php";s:4:"65a3";s:17:"pi1/locallang.xml";s:4:"695e";s:15:"res/ico_del.gif";s:4:"0deb";s:16:"res/ico_edit.gif";s:4:"4a05";s:21:"res/tmpl_default.html";s:4:"b0e4";s:23:"static/ts/constants.txt";s:4:"e383";s:19:"static/ts/setup.txt";s:4:"63b7";}',
+);
+
+?>
