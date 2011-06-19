da6734ff4542730e909e74b37a6463089a25a20d
commodore <commodore@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-19 16:19:20 +0000

    Flexform renderMode "tree" for T3 v4.5
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/elemente_fenews/trunk@48877 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -2,31 +2,37 @@
 	if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 	t3lib_extMgm::addStaticFile($_EXTKEY,'static/ts/','Frontend News');
+	t3lib_extMgm::addStaticFile($_EXTKEY,'static/css/','Frontend News Default Styles');
 
-/*	if (TYPO3_MODE=='BE')	{
-		t3lib_extMgm::insertModuleFunction(
+	if (TYPO3_MODE=='BE')	{
+/*		t3lib_extMgm::insertModuleFunction(
 			'user_task',
 			'tx_elementefenews_modfunc1',
 			t3lib_extMgm::extPath($_EXTKEY).'modfunc1/class.tx_elementefenews_modfunc1.php',
 			'LLL:EXT:elemente_fenews/locallang_db.xml:moduleFunction.tx_elementefenews_modfunc1'
 		);
-		$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_elementefenews_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_elementefenews_pi1_wizicon.php';
+*/		$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_elementefenews_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_elementefenews_pi1_wizicon.php';
 	}
-*/
+
 	
 	t3lib_div::loadTCA('tt_content');
 	$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages,recursive';
 	$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
 
-	// tt_news version
+
+	// Different flexform config for tt_news v2.5.x and v3.0.x
 	require_once(t3lib_extMgm::extPath($_EXTKEY).'lib/class.tx_elementefenews_version.php');
 	switch(tx_elementefenews_version::getEM_CONFVersion('tt_news')) {
 		case '3.0':
 			t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds30.xml');		
 		break;
 		default:
-			t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds.xml');
+			t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds25.xml');
 		break;
+	}
+	// If TYPO3 v4.5 is the host, use new renderMode "tree"
+	if (t3lib_div::int_from_ver(TYPO3_version) >= 4005000) {
+		t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds.xml');
 	}
 	
 	t3lib_extMgm::addPlugin(array('LLL:EXT:elemente_fenews/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
